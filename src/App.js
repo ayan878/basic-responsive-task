559727caa4832c8c052717256f5977b21d2a5cc9
ayan878 <ayanraza197@gmail.com>
2024-05-27 20:49:07 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./component/Header";
+import Hero from "./component/Hero";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero/>
     </div>
   );
 }
