b923d80f42fe14345da5df00ce8585d2bacb7a4f
ayan878 <ayanraza197@gmail.com>
2024-05-28 15:55:25 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./component/Header";
 import Hero from "./component/Hero";
+import Accordion from "./component/Accordion";
+import AddYourOwn from "./component/AddYourOwn";
+import Testimonial from "./component/Testimonial";
+import YourHobby from "./component/YourHobby";
+import Footer from "./component/Footer";
+import MobileSignIn from "./component/MobileSignIn";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Hero/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/sign-in" element={<MobileSignIn />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Hero />
+                <Accordion />
+                <AddYourOwn />
+                <Testimonial />
+                <YourHobby />
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
